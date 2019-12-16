5bf1bd0a3f4a4dd8a16745634527f777d73e9850
CarlKarlQarl <karlmatthes@gmail.com>
2019-12-16 11:07:22 -0700

    First commit

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,4 +3,14 @@ class CarsController < ApplicationController
     @cars = Car.all
     render json: @cars
   end
+
+  def create
+    Car.create(
+      make:params[:make],
+      model:params[:model],
+      year:params[:year],
+      rating:params[:rating]
+    )
+    render json: {message:"Added new car"}
+  end
 end
